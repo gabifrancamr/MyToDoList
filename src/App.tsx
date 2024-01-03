b1e2089d184586be4e7136bdc443b7ffeb96bda3
Gabriela França <gabrielafranca2008@hotmail.com>
2024-01-03 19:33:55 -0300

    elaborando tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header'
-import { PlusCircle } from 'phosphor-react'
+import { PlusCircle, Trash } from 'phosphor-react'
+import { v4 as uuidv4 } from 'uuid'
 import './global.module.css'
 
 import styles from './App.module.css'
@@ -7,17 +8,34 @@ import { ListHeader } from './components/ListHeader'
 import { WithoutTasks } from './components/WithoutTasks'
 import { ChangeEvent, useState } from 'react'
 
+interface InfoTasks {
+  task: string,
+  
+}
+
 export function App() {
   const [inputNewTask, setInputNewTask] = useState('')
+  const [tasks, setTasks] = useState<string[]>([])
+  const [isChecked, setIsChecked] = useState(false)
 
   function handleInputNewTaskChange(e:ChangeEvent<HTMLInputElement>) {
     setInputNewTask(e.target.value)
   }
 
   function handleCreateNewTask() {
-    alert(inputNewTask)
-    setInputNewTask("")
+    if(inputNewTask) {
+      setTasks([...tasks, inputNewTask])
+      setInputNewTask("")
+    } else {
+      alert("Digite alguma tarefa!")
+    }
   }
+
+  function handleChangeChecked() {
+    setIsChecked(!isChecked)
+  }
+
+  
 
   return (
     <div>
@@ -39,8 +57,27 @@ export function App() {
 
         <div className={styles.areaTasks}>
           <ListHeader />
-
-          <WithoutTasks/>
+          
+          {tasks.length === 0 ? (
+            <WithoutTasks/>
+          ) : (
+            <div className={styles.allTasks}>
+              {tasks.map((task) => {
+                const uniId = uuidv4()
+                return (
+                  <div className={styles.taskBox} >
+                    <div className={styles.clickBox} onClick={handleChangeChecked}>
+                      <input className={styles.taskRadio} type="radio" id={uniId} checked={isChecked} />
+                    <label className={styles.taskContent} htmlFor={uniId}>{task}</label>
+                    </div>
+                    
+                    <Trash />
+                  </div>
+                )
+              })}
+            </div>
+          )}
+          
         </div>
       </section>
     </div>
