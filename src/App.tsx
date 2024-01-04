8911f274bd95bca3e4d73f8611ef3807688a5a22
Gabriela França <gabrielafranca2008@hotmail.com>
2024-01-03 21:59:46 -0300

    criação do componente ListTasksModules e criação da função de deletar comentário usado id

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,102 @@
-import { Header } from './components/Header'
-import { PlusCircle, Trash } from 'phosphor-react'
-import { v4 as uuidv4 } from 'uuid'
-import './global.module.css'
+import { Header } from "./components/Header";
+import { PlusCircle } from "phosphor-react";
+import { v4 as uuidv4 } from "uuid";
+import "./global.module.css";
 
-import styles from './App.module.css'
-import { ListHeader } from './components/ListHeader'
-import { WithoutTasks } from './components/WithoutTasks'
-import { ChangeEvent, useState } from 'react'
+import styles from "./App.module.css";
+import { ListHeader } from "./components/ListHeader";
+import { WithoutTasks } from "./components/WithoutTasks";
+import { ChangeEvent, useState } from "react";
+import { ListTasks } from "./components/ListTasks";
 
 interface InfoTasks {
-  task: string,
-  id: string,
-  isChecked: boolean
+  task: string;
+  id: string;
+  isChecked: boolean;
 }
 
 export function App() {
-  const [inputNewTask, setInputNewTask] = useState('')
-  const [tasks, setTasks] = useState<InfoTasks[]>([])
- 
+  const [inputNewTask, setInputNewTask] = useState("");
+  const [tasks, setTasks] = useState<InfoTasks[]>([]);
 
-  function handleInputNewTaskChange(e:ChangeEvent<HTMLInputElement>) {
-    setInputNewTask(e.target.value)
+  function handleInputNewTaskChange(e: ChangeEvent<HTMLInputElement>) {
+    setInputNewTask(e.target.value);
   }
 
   function handleCreateNewTask() {
-    if(inputNewTask) {
-      const newTask:InfoTasks = {
+    if (inputNewTask) {
+      const newTask: InfoTasks = {
         id: uuidv4(),
         task: inputNewTask,
-        isChecked: false
-      }
-      setTasks((state) => [...state, newTask])
-      setInputNewTask("")
+        isChecked: false,
+      };
+      setTasks((state) => [...state, newTask]);
+      setInputNewTask("");
     } else {
-      alert("Digite alguma tarefa!")
+      alert("Digite alguma tarefa!");
     }
   }
 
   function handleChangeChecked(taskId: string) {
     setTasks(
-      tasks.map((task) => task.id === taskId ? { ...task, isChecked: !task.isChecked } : task)
-    )
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, isChecked: !task.isChecked } : task
+      )
+    );
   }
 
-  
+  function deleteTask(id: string) {
+    const newTasks = tasks.filter((task) => task.id !== id)
+
+    setTasks(newTasks)
+  }
 
   return (
     <div>
       <Header />
-      
+
       <section className={styles.mainContent}>
         <div className={styles.newTask}>
-          <input 
-            type="text" 
-            placeholder='Adicione uma nova tarefa' 
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
             value={inputNewTask}
             onChange={handleInputNewTaskChange}
           />
-          <button className={styles.newTaskButton} onClick={handleCreateNewTask}>
+          <button
+            className={styles.newTaskButton}
+            onClick={handleCreateNewTask}
+          >
             <span>Criar</span>
-            <PlusCircle size={20}/>
+            <PlusCircle size={20} />
           </button>
         </div>
 
         <div className={styles.areaTasks}>
           <ListHeader />
-          
+
           {tasks.length === 0 ? (
-            <WithoutTasks/>
+            <WithoutTasks />
           ) : (
             <div className={styles.allTasks}>
               {tasks.map((task) => {
                 return (
-                  <div className={styles.taskBox} key={task.id} >
-                    <div className={styles.clickBox} onChange={() => handleChangeChecked(task.id)}>
-                      <input className={styles.taskRadio} type="checkbox" id={task.id} checked={task.isChecked} />
-                    <label className={styles.taskContent} htmlFor={task.id}>{task.task}</label>
-                    </div>
-                    
-                    <Trash />
-                  </div>
-                )
+                  <>
+                    <ListTasks
+                      key={task.id}
+                      id={task.id}
+                      task={task.task}
+                      isChecked={task.isChecked}
+                      onHandleChangeChecked={handleChangeChecked}
+                      onDeleteTask={deleteTask}
+                    />
+                  </>
+                );
               })}
             </div>
           )}
-          
         </div>
       </section>
     </div>
-  )
+  );
 }
---
+++ src/components/ListTasks.tsx
@@ -0,0 +1,51 @@
+import { Trash } from "phosphor-react";
+import styles from "./ListTasks.module.css";
+
+interface PropsListTasks {
+  id: string;
+  task: string;
+  isChecked: boolean;
+  onHandleChangeChecked: (id: string) => void;
+  onDeleteTask: (id: string) => void
+}
+
+export function ListTasks({
+  id,
+  task,
+  isChecked,
+  onHandleChangeChecked,
+  onDeleteTask
+}: PropsListTasks) {
+  function changeChecked() {
+    onHandleChangeChecked(id);
+  }
+
+  function handleDeleteTask() {
+    const confirmation = confirm("Deseja apagar tarefa?")
+
+    if(confirmation) {
+        onDeleteTask(id)
+    }
+  }
+
+  return (
+    <div className={styles.taskBox} id={id}>
+      <div className={styles.clickBox} onChange={changeChecked}>
+        <input
+          className={styles.taskRadio}
+          type="checkbox"
+          id={id}
+          checked={isChecked}
+        />
+        <label
+          className={`${styles.taskContent} ${isChecked ? styles.checked : ""}`}
+          htmlFor={id}
+        >
+          {task}
+        </label>
+      </div>
+
+      <Trash className={styles.trash} size={20} onClick={handleDeleteTask}/>
+    </div>
+  );
+}
