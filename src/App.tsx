db2f32330abaee0e2760c14e6968e432b94500d1
Gabriela <gabifmouraroc@outlook.com>
2024-06-09 17:51:20 -0300

    formulário de novas tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Header } from "./components/Header";
 import { PlusCircle } from "phosphor-react";
 import { v4 as uuidv4 } from "uuid";
 import "./global.module.css";
-import "./index.css"
+import "./index.css";
 
 import styles from "./App.module.css";
 import { ListHeader } from "./components/ListHeader";
 import { WithoutTasks } from "./components/WithoutTasks";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { ListTasks } from "./components/ListTasks";
 
 interface InfoTasks {
@@ -25,7 +25,8 @@ export function App() {
     setInputNewTask(e.target.value);
   }
 
-  function handleCreateNewTask() {
+  function handleCreateNewTask(e: FormEvent) {
+    e.preventDefault();
     if (inputNewTask) {
       const newTask: InfoTasks = {
         id: uuidv4(),
@@ -64,24 +65,21 @@ export function App() {
   return (
     <div>
       <Header />
-      
+
       <section className={styles.mainContent}>
-        <div className={styles.newTask}>
+        <form className={styles.newTask} onSubmit={handleCreateNewTask}>
           <input
             type="text"
+            required
             placeholder="Adicione uma nova tarefa"
             value={inputNewTask}
             onChange={handleInputNewTaskChange}
-            id={uuidv4()}
           />
-          <button
-            className={styles.newTaskButton}
-            onClick={handleCreateNewTask}
-          >
+          <button type="submit" className={styles.newTaskButton}>
             <span>Criar</span>
             <PlusCircle size={20} />
           </button>
-        </div>
+        </form>
 
         <div className={styles.areaTasks}>
           <ListHeader
